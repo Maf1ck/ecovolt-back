da84609249a5ee70d0a524de4eff46455beab238
maf1ck <kosmanmatvij@gmail.com>
2025-08-11 12:28:50 +0300

    add pages limit

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,15 +2,23 @@ import axios from "axios";
 import { config } from "../config/env.js";
 
 export const getProducts = async (req, res) => {
+  const { limit = 8, last_id } = req.query;
+
   try {
     const response = await axios.get(
       "https://my.prom.ua/api/v1/products/list",
       {
         headers: {
           Authorization: `Bearer ${config.promApiToken}`,
+          "X-LANGUAGE": "uk",
+        },
+        params: {
+          limit,
+          ...(last_id ? { last_id } : {}),
         },
       }
     );
+
     res.json(response.data);
   } catch (error) {
     console.error("❌ Error fetching products:", error.message);
